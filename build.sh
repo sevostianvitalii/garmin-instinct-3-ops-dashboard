d9793ad7a4e6e1a2b2606a1afb96a897d63b63d0
user <user@kalix240.lan>
2026-02-01 15:17:41 +0200

    Fix: Add Instinct 3 targets and build specific binary for I3 Solar

--- build.sh
+++ build.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 SDK_BIN="/home/user/.Garmin/ConnectIQ/Sdks/connectiq-sdk-lin-8.4.0-2025-12-03-5122605dc/bin"
+
+# Build specifically for the device likely used (Instinct 3 Solar is common, or just build generic export)
+# Using -e (export) creates an .iq file, but for sideloading we need a .prg
+# We must specify the EXACT target device for a PRG sideload.
+# A .prg built for 'instinct2' WILL NOT RUN on 'instinct3'.
+
+echo "Building for Instinct 3 Solar 45mm..."
+$SDK_BIN/monkeyc \
+    -o OpsDashboard_I3Solar.prg \
+    -f monkey.jungle \
+    -y developer_key.der \
+    -d instinct3solar45mm \
+    -w
+
+echo "Building for Instinct 2..."
 $SDK_BIN/monkeyc \
     -o OpsDashboard.prg \
     -f monkey.jungle \
     -y developer_key.der \
     -d instinct2 \
     -w
+
+echo "Done. Use OpsDashboard_I3Solar.prg for Instinct 3 Solar."
