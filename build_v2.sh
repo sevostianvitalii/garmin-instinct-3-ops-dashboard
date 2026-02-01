ba8fb8830a9744df33c508a1d19db8e82ca39931
user <user@kalix240.lan>
2026-02-01 17:22:32 +0200

    Fix: Rebuild V2 using native I3 target for visibility + Fixed Layout

--- build_v2.sh
+++ build_v2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 SDK_BIN="/home/user/.Garmin/ConnectIQ/Sdks/connectiq-sdk-lin-8.4.0-2025-12-03-5122605dc/bin"
 
-echo "Building v2 for Instinct 2X / 3 Standard (Universal)..."
-# Generating a v2 binary to distinguish for user
+echo "Building v2 (Native I3 Solar) for fit tests..."
+# Using the native I3 target (which worked before) but with the NEW layout and NEW AppID
 $SDK_BIN/monkeyc \
-    -o OpsDashboard_v2_I2X.prg \
+    -o OpsDashboard_v2_I3Solar.prg \
     -f monkey.jungle \
     -y developer_key.der \
-    -d instinct2x \
+    -d instinct3solar45mm \
     -w
 
-echo "Done. Use OpsDashboard_v2_I2X.prg"
+echo "Done. Use OpsDashboard_v2_I3Solar.prg"
